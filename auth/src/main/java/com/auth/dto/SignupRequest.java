bb45334960a867e54ed4d6bb179388f055b2ec24
AzerArfa <120283616+AzerArfa@users.noreply.github.com>
2024-05-05 13:41:45 +0200

    Add files via upload

---
+++ auth/src/main/java/com/auth/dto/SignupRequest.java
@@ -0,0 +1,15 @@
+package com.auth.dto;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class SignupRequest {
+
+    private String email;
+    private String password;
+    private String name;
+    private String societe;
+    private byte[] img;
+}
