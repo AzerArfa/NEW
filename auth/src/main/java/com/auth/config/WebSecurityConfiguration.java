9f98804af69e172c099a9c56ee1ac139e1c7d7cd
AzerArfa <120283616+AzerArfa@users.noreply.github.com>
2024-05-05 04:24:54 +0200

    Add files via upload

---
+++ auth/src/main/java/com/auth/config/WebSecurityConfiguration.java
@@ -0,0 +1,81 @@
+package com.auth.config;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfiguration {
+
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	    return http
+	            .csrf().disable()
+	            .authorizeHttpRequests(auth -> auth
+	            	    .requestMatchers("/auth/signup").permitAll()
+	            	    .requestMatchers("/auth/users").permitAll()
+	            	    .requestMatchers("/auth/delete/**").permitAll()
+	            	    .requestMatchers("/auth/login").permitAll()
+	            	    .requestMatchers(HttpMethod.PUT,"/auth/user/**").permitAll()
+	            	    .requestMatchers("/auth/updatepassword").permitAll()
+	            	    .requestMatchers("/auth/make-user/**").hasAuthority("SUPERADMIN")
+	            	    .requestMatchers("/auth/make-admin/**").hasAuthority("SUPERADMIN")
+	            	    .anyRequest().authenticated()
+	            	)
+	            .sessionManagement()
+	            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+	            .and()
+	            .build();
+	}
+
+
+	
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+}
